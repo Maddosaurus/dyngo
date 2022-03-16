11307168752795c82837f998423a2259e8d12528
Matthias <gh@maddosaurus.de>
2022-03-16 19:42:28 +0100

    Introduce INWX basic dyn updater

---
+++ cmd/dyngo/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+
+	externalip "github.com/glendc/go-external-ip"
+	"github.com/nrdcg/goinwx"
+)
+
+func get_env_value(env_var string) string {
+	ret := os.Getenv(env_var)
+	if ret != "" {
+		return ret
+	}
+	fmt.Printf("Error! Did not find required environment variable %v\n", env_var)
+	os.Exit(1)
+	return ""
+}
+
+func determineExtIP() net.IP {
+	consensus := externalip.DefaultConsensus(nil, nil)
+	consensus.UseIPProtocol(4)
+
+	ip, err := consensus.ExternalIP()
+	if err != nil {
+		fmt.Println("Error retrieving external IP!")
+		os.Exit(1)
+	}
+	fmt.Printf("Found external IP: %v\n", ip.To4().String())
+	return ip
+}
+
+func removeSubdomain(fullTLD string) string {
+	domain := strings.Split(fullTLD, ".")
+	ret := domain[1] + "." + domain[2]
+	fmt.Println(ret)
+	return ret
+}
+
+func getSubdomainRecord(subdomain string, client *goinwx.Client) (goinwx.NameserverRecord, error) {
+	var req = &goinwx.NameserverInfoRequest{
+		Domain: removeSubdomain(subdomain),
+	}
+
+	resp, _ := client.Nameservers.Info(req) // FIXME: Error handling
+	for _, child := range resp.Records {
+		if child.Name == subdomain {
+			fmt.Printf("Found Subdomain: %v\n", child)
+			return child, nil
+		}
+	}
+	return *&goinwx.NameserverRecord{}, errors.New("Did not find subdomain!")
+}
+
+func updateRecord(user string, pass string, record string, address string) {
+	client := goinwx.NewClient(user, pass, &goinwx.ClientOptions{})
+	_, err := client.Account.Login()
+	if err != nil {
+		log.Fatalf("INWX: Failed login:\n%v\n", err)
+	}
+	defer func() {
+		if err := client.Account.Logout(); err != nil {
+			log.Fatalf("INWX: Failed logout:\n%v\n", err)
+		}
+	}()
+
+	subdomain, err := getSubdomainRecord(record, client)
+	if err != nil {
+		log.Fatalf("INWX: Error while finding subdomain: %v\n", err)
+		os.Exit(1)
+	}
+
+	var request = &goinwx.NameserverRecordRequest{
+		Name:    subdomain.Name,
+		Type:    subdomain.Type,
+		Content: address,
+		TTL:     301,
+	}
+
+	err = client.Nameservers.UpdateRecord(subdomain.ID, request)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	username := get_env_value("INWX_USERNAME")
+	password := get_env_value("INWX_PASSWORD")
+	domain_record := get_env_value("INWX_DOMAIN_RECORD")
+	ip_v4 := determineExtIP().To4().String()
+
+	fmt.Printf("Found data: %v for %v - IP: %v\n\n", username, domain_record, ip_v4)
+
+	updateRecord(username, password, domain_record, ip_v4)
+}
---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/maddosaurus/dyngo
+
+go 1.18
+
+require (
+	github.com/fatih/structs v1.1.0 // indirect
+	github.com/glendc/go-external-ip v0.1.0 // indirect
+	github.com/kolo/xmlrpc v0.0.0-20200310150728-e0350524596b // indirect
+	github.com/mitchellh/mapstructure v1.3.3 // indirect
+	github.com/nrdcg/goinwx v0.8.1 // indirect
+)
